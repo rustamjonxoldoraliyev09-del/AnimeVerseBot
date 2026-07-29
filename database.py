7a9c64a329350d7fa5628535d87b5b3d2eadf316
rustamjonxoldoraliyev09-del <rustamjonxoldoraliyev09@gmail.com>
2026-07-29 10:06:08 +0500

    Create database.py1

---
+++ database.py
@@ -0,0 +1,27 @@
+# MONGO KODINGIZNI QO'YING VA PAROLINGIZNI YOZING!
+MONGO_URL = "mongodb+srv://rustambek:qwertyupoi98@clunster0.xxxxx.mongodb.net/?retryWrites=true&w=majority"
+
+client = MongoClient(MONGO_URL)
+db = client["anime_verse_database"]
+
+# Jadvallar (Kolleksiyalar)
+animes_col = db["animes"]
+episodes_col = db["episodes"]
+
+def init_db():
+    """ Test uchun 101 kodli animeni avtomatik bazaga qo'shish """
+    anime = animes_col.find_one({"_id": "101"})
+    if not anime:
+        anime_data = {
+            "_id": "101",
+            "title": "Shilliq sifatida qayta tug'ilganim haqida (1-fasl)",
+            "photo": "https://justwatch.com",
+            "episodes_count": 24,
+            "country": "Yaponiya",
+            "language": "O'ZBEK tilida",
+            "year": "2018",
+            "genre": "Ekshn, Komediya, Fentezi, O'zga Dunyo",
+            "views": "17366",
+            "channel_link": "@an1verseuz"
+        }
+        animes_col.insert_one(anime_data
